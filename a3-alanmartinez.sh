75ae83fc3a125bb3c1d50cde1dc55df88d59b6a5
Alan <alma031@vidalibarraquer.net>
2022-03-01 12:57:33 +0100

    Programa acabado.

--- a3-alanmartinez.sh
+++ a3-alanmartinez.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 #
-# Activitat 3 - Usuaris
+# a3-alanmartinez.sh
 #
-sudo echo "--- Introdueix un nom d'usuari ---"
+clear
+sudo echo "--- Benvingut/da al programa de creació d'usuaris ---"
+echo "Introdueix un nom d'usuari (escrigui 0 per sortir):"
 read user
+if [[ $user == 0 ]]; then
+  echo "Ha volgut sortir."
+  exit 115
+fi
 if grep -w $user /etc/passwd; then
   echo "L'usuari $user existeix."
 else
@@ -12,9 +18,10 @@ else
   read crear
   if [[ $crear = s ]]; then
     echo "Ha decidit donar-lo d'alta."
-    sudo adduser --disabled-password $user
+    sudo useradd -m $user
     echo "Usuari $user registrat."
   elif [[ $crear = n ]]; then
     echo "Ha decidit no donar-lo d'alta."
+    echo "Nom de l'usuari no creat: $user" >> errors.log
   fi
 fi
